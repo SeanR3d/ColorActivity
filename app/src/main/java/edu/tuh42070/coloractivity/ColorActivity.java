1a087dd59aadae0b58e7276a26017a6ba642e91e
Sean <SeanR1194@gmail.com>
2019-09-25 21:50:19 -0400

    Added spinner with working custom adapter

---
+++ app/src/main/java/edu/tuh42070/coloractivity/ColorActivity.java
@@ -0,0 +1,59 @@
+package edu.tuh42070.coloractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class ColorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] colors = {"Select a color", "Red", "Yellow", "Green", "Teal", "Cyan", "Blue",
+                        "Magenta", "Purple", "Maroon", "Black", "Gray"};
+
+        // Create Adapter
+        ColorAdapter colorAdapter = new ColorAdapter(ColorActivity.this, colors);
+
+        // Obtain spinner
+        Spinner colorSpinner = findViewById(R.id.spinner);
+        colorSpinner.setBackgroundColor(Color.DKGRAY);
+        findViewById(R.id.layout).setBackgroundColor(Color.WHITE);
+        colorSpinner.setAdapter(colorAdapter);
+
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                //Change textview to white
+                view.setBackgroundColor(Color.WHITE);
+
+                //set background to color
+                ConstraintLayout layout = findViewById(R.id.layout);
+
+                String colorValue = ((TextView) view).getText().toString();
+                if (colorValue.equals("Select a color")) colorValue = "White";
+
+                layout.setBackgroundColor(Color.parseColor(colorValue));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                findViewById(R.id.layout).setBackgroundColor(Color.WHITE);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/edu/tuh42070/coloractivity/ColorAdapter.java
@@ -0,0 +1,67 @@
+package edu.tuh42070.coloractivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+class ColorAdapter extends BaseAdapter {
+
+    Context context;
+    String[] colors;
+
+    public ColorAdapter(Context context, String[] colors){
+        this.context = context;
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return colors[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView textView;
+        String colorValue;
+
+        // Check for recycle view
+        if (convertView != null && convertView instanceof TextView)
+            textView = (TextView) convertView;
+        else
+            textView = new TextView(this.context);
+
+        // Change the default, "Select a color", color value to white
+        if (position == 0)
+            colorValue = "TRANSPARENT";
+        else
+            colorValue = this.colors[position];
+        // Change the black view's text to light grey for visibility
+
+        if (colorValue.equals("Black"))
+            textView.setTextColor(Color.LTGRAY);
+        else
+            textView.setTextColor(Color.BLACK);
+
+        // Set view configs
+        textView.setText(this.colors[position]);
+        textView.setBackgroundColor(Color.parseColor(colorValue));
+        textView.setTextSize(25);
+        textView.setPadding(5,5,0,5);
+
+        return textView;
+    }
+}
